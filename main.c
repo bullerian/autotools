26ef38ac4bb118f67ee3f9705c188c1ce1607cd4
Dmytro <lizometrix@gmail.com>
2017-06-11 13:42:17 +0300

    Working makefile

--- main.c
+++ main.c
@@ -8,10 +8,10 @@ int main(void){
     
     printf("\n%s\n", "Calculate geometrical values");
     for(int i=0;i<VALUES_SIZE;i++){
-        printf("%s%f%10.3f\n", "Circle area r=", values[i], circleArea(values[i]));
-        printf("%s%f%10.3f\n", "Rectangle area side=", values[i], rectangleArea(values[i]));
-        printf("%s%f%10.3f\n", "Sphere volume r=", values[i], sphereVolume(values[i]));
-        printf("%s%f%10.3f\n", "Rectangle volume r=", values[i], rectangleVolume(values[i]));
+        printf("%s%f A=%10.3f\n", "Circle area r=", values[i], circleArea(values[i]));
+        printf("%s%f A=%10.3f\n", "Rectangle area side=", values[i], rectangleArea(values[i]));
+        printf("%s%f V=%10.3f\n", "Sphere volume r=", values[i], sphereVolume(values[i]));
+        printf("%s%f V=%10.3f\n", "Rectangle volume r=", values[i], rectangleVolume(values[i]));
     }      
     
     return 0; 
